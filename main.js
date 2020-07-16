233a68f39ee07b61a9c664910d82e8387bc6ae5a
Alex Marmolejo <alex97marmolejo@gmail.com>
2020-07-16 02:35:58 -0400

    added functionality to rgb button which produces a random color trail.

--- main.js
+++ main.js
@@ -6,12 +6,16 @@ const resetButton = document.createElement('button');
 const rgbButton = document.createElement('button');
 const gridSquares = [];
 let area = 16;
+let r = undefined;
+let g = undefined;
+let b = undefined;
 
 const runFunctions = () => {
     appendTags();
     styleContent();
     generateGrid();
     updateContainer();
+    toggle();
     resetGrid();
 }
 
@@ -22,6 +26,7 @@ const appendTags = function() {
     bodyElement.appendChild(mainTag);
     mainTag.appendChild(mainContainer);
     mainContainer.classList = 'main-container';
+    rgbButton.setAttribute('value', 'off');
 };
 
 const styleContent = function() {
@@ -42,10 +47,14 @@ const generateGrid = function() {
         gridSquares[i] = document.createElement('div');
         gridSquares[i].classList = 'grid-div';
         mainContainer.append(gridSquares[i]);
-        gridSquares[i].addEventListener('mousemove', (event) => {
-            return event.target.style.background = "white"
+        gridSquares[i].addEventListener('mouseleave', (event) => {
+            if(rgbButton.value === 'off') {
+                return event.target.style.background = "white";
+            } else {
+                return event.target.style.background = randomRGB();
+            };
         });
-    }
+    };
 };
 
 const resetGrid = function() {
@@ -66,28 +75,50 @@ const generateNewGrid = function(e) {
         updateContainer();
     } else {
         return alert('invalid input');
-    }
+    };
     console.log(e);
 };
 
+const randomRGB = function() {
+    getRandomInt = () => {
+        return Math.floor(Math.random() * Math.floor(255));
+      };
+    console.log(`${r = getRandomInt()} ${g = getRandomInt()} ${b = getRandomInt()}`);
+    return `rgb(${r},${g},${b})`;
+};
+
+const toggle = function() {
+    rgbButton.addEventListener('click', (e) => {
+        if(e.target.value==="off"){
+            e.target.value="on";
+            e.target.style.background = `linear-gradient(45deg,${randomRGB()},${randomRGB()})`;
+        }
+        else if(e.target.value==="on"){
+            e.target.value="off";
+            e.target.style.background = '#00b7ff';
+        };
+    });
+};
+
+
 const styleResetButton = function() {
     resetButton.textContent = 'reset';
-    resetButton.style.fontFamily = '\'Indie Flower\', cursive'
-    resetButton.style.fontSize = '16px'
-    resetButton.style.background = '#00b7ff'
+    resetButton.style.fontFamily = '\'Indie Flower\', cursive';
+    resetButton.style.fontSize = '16px';
+    resetButton.style.background = '#00b7ff';
     resetButton.style.border = 'none';
     resetButton.style.borderRadius = '15px';
     resetButton.style.display = 'block';
-    resetButton.style.width = '500px'
+    resetButton.style.width = '500px';
     resetButton.style.margin = '10px auto';
     resetButton.style.padding = '10px 0';
 };
 
 const styleRgbButton = function() {
     rgbButton.textContent = 'RGB';
-    rgbButton.style.fontFamily = '\'Indie Flower\', cursive'
-    rgbButton.style.fontSize = '16px'
-    rgbButton.style.background = '#00b7ff'
+    rgbButton.style.fontFamily = '\'Indie Flower\', cursive';
+    rgbButton.style.fontSize = '16px';
+    rgbButton.style.background = '#00b7ff';
     rgbButton.style.border = 'none';
     rgbButton.style.borderRadius = '15px';
     rgbButton.style.display = 'block';
